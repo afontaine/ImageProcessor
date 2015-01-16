7a6bc1cca57100fc2ee77d5dec760c83d076ffb9
Andrew Fontaine <andrew@afontaine.ca>
2015-01-16 14:35:21 -0700

    Image selection moved to Action Bar because ACTION

--- app/src/main/java/ca/afontaine/imageprocessor/app/ImageProcessor.java
+++ app/src/main/java/ca/afontaine/imageprocessor/app/ImageProcessor.java
@@ -1,17 +1,28 @@
 package ca.afontaine.imageprocessor.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 
-public class ImageProcessor extends ActionBarActivity {
+public class ImageProcessor extends Activity {
+
+    private static final String TAG = "ImageProcessor";
+    private static final int GET_IMAGE = 1;
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_processor);
+
+        image = (ImageView) findViewById(R.id.imageView);
+
     }
 
 
@@ -27,13 +38,28 @@ public class ImageProcessor extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.image:
+                setImage();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setImage() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, GET_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != RESULT_OK)
+            return;
+
+        if(requestCode == GET_IMAGE) {
+            image.setImageURI(data.getData());
+        }
     }
 }
